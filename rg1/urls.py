3156c22bc4fea438118555d24d315d851c4e7bdd
Leandro Passos <lpkyrius@gmail.com>
2020-09-16 15:32:36 -0300

    ajuste branch-work

--- rg1/urls.py
+++ rg1/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# Relação URLs tuilizadas neste app
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
